2fcd7ed794b9dfe2480563c4da5daf2831dcb536
ajay <ajay@ubuntu22.04.myguest.virtualbox.org>
2023-10-27 17:44:42 +0530

    CI4.x Database Seeding Ex:

---
+++ ci4/app/Database/Migrations/2023-10-27-115443_Student.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Student extends Migration
+{
+    public function up()
+    {
+            $this->forge->addField([
+                
+                    'id' => [
+                        'type'           => 'INT',
+                        'constraint'     => 5,
+                        'unsigned'       => true,
+                        'auto_increment' => true,
+                    ],
+                    'name' => [
+                        'type'       => 'VARCHAR',
+                        'constraint' => '100',
+                    ],
+                    'address' => [
+                        'type' => 'TEXT',
+                        'null' => true,
+                    ],
+                    'created_at' => [
+                        'type' => 'DATETIME',
+                        'null' => true,
+                    ],
+                    'updated_at' => [
+                        'type' => 'DATETIME',
+                        'null' => true,
+                    ],                        
+                ]);
+
+                 $this->forge->addKey('id', true);
+                 $this->forge->createTable('students');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('students');
+    }
+}
